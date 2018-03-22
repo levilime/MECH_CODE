4e5776cb893227c938922faff2a26d361c57f45a
hmyeh <h.m.yeh@student.tudelft.nl>
2018-03-22 19:24:54 +0100

    Emit Message Events

--- mirror/src/caster/multicaster.js
+++ mirror/src/caster/multicaster.js
@@ -1,8 +1,9 @@
 const dgram = require('dgram');
-
+const events = require('events');
 const BROADCAST_ADDRESS = '255.255.255.255';
 
 //TODO logging of actions
+// Message: {topic/type, content}
 
 class Multicaster{
 
@@ -10,6 +11,7 @@ class Multicaster{
         this.port = port;
         this.receiver = this.initReceiver();
         this.sender = this.initSender();
+        this.eventEmitter = new events.EventEmitter();
     }
 
     /**
@@ -20,6 +22,7 @@ class Multicaster{
         const receiver = dgram.createSocket({type:'udp4', reuseAddr:true});
 
         receiver.on('message', (msg, rinfo) => {
+            this.receiveMessage(msg, rinfo);
             console.log('Receiver got: ' + msg + ' from ' + rinfo.address + ':' + rinfo.port);
         });
 
@@ -41,6 +44,23 @@ class Multicaster{
     }
 
     /**
+     * Emit the topic and the content of the message
+     * For heartbeat messages, add the sender information
+     * @param msg
+     * @param rinfo
+     */
+    receiveMessage(msg, rinfo) {
+        const parsedMsg = JSON.parse(msg.toString('utf8'));
+        if (parsedMsg.topic === 'HEARTBEAT') {
+            const content = parsedMsg.content;
+            content.rinfo = rinfo;
+            this.eventEmitter.emit(parsedMsg.topic, content);
+        } else {
+            this.eventEmitter.emit(parsedMsg.topic, parsedMsg.content);
+        }
+    }
+
+    /**
      * Initialize the Sender UDP socket on a random port and address
      * @returns {*}
      */
@@ -48,15 +68,15 @@ class Multicaster{
         const sender = dgram.createSocket('udp4');
         sender.on('listening', () => {
             sender.setBroadcast(true);
-            console.log('')
+            console.log('Sender listening');
         });
 
         sender.on('error', (error) => {
-            console.log('Receiver error: ' + error);
+            console.log('Sender error: ' + error);
         });
 
         sender.on('close', () => {
-            console.log('Receiver closed');
+            console.log('Sender closed');
         });
 
         sender.bind();
@@ -65,10 +85,11 @@ class Multicaster{
 
     /**
      * Use the Sender socket to broadcast the message on the Broadcast Address and given port
-     * @param msg
+     * @param topic
+     * @param content
      */
-    sendMessage(msg) {
-        const message = new Buffer(msg);
+    sendMessage(topic, content) {
+        const message = new Buffer(JSON.stringify({topic, content}));
         this.sender.send(message, 0, message.length, this.port, BROADCAST_ADDRESS);
     }
 
@@ -76,5 +97,10 @@ class Multicaster{
         this.receiver.close();
         this.sender.close();
     }
+
+    getEventEmitter() {
+        return this.eventEmitter;
+    }
 }
 
+module.exports = {Multicaster};
