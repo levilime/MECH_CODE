2f5faaaf677d0b521c80dfd7a17ce2c56b990f19
hmyeh <h.m.yeh@student.tudelft.nl>
2018-03-29 00:19:20 +0200

    Fix Lamport test

--- mirror/src/time/lamport.js
+++ mirror/src/time/lamport.js
@@ -3,6 +3,7 @@ const timemachine = require('timemachine');
 const lamportClock = (timestamp) => {
   if (timestamp > Date.now()) {
     timemachine.config({timestamp:timestamp+1, tick:true});
+    return timestamp + 1;
   }
   return Date.now();
 };
