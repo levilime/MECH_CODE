de07bfc0850e3d96f5509f267007748d0324400e
levilime <levilime@gmail.com>
2018-11-05 14:09:39 +0100

    Some adjustements to put it open source.
    - Added status message in client.
    
    The code is currently not yet correct due to how the NPCs are implemented,
    they create an inconsistent state between servers.
    If the NPCs are implemented as players than the system will work as expected.

--- clientagent/main.js
+++ clientagent/main.js
@@ -2,10 +2,11 @@ const ClientAgent = require('./clientagent');
 const BaseAddress = 'http://localhost:300';
 const knownAdresses = Array(1).fill(0).map((x,i) => BaseAddress + i);
 
-const amountOfAgents = 20;
+const amountOfAgents = 40;
 
 const initialize = () =>  {
     Array(amountOfAgents).fill('').forEach(() => {
+        console.log(knownAdresses);
         new ClientAgent(knownAdresses);
     });
 };
